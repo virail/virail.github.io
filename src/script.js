d285cb9983824ab1cad1462e811bf82c9c048281
virail <xjbviralx@gmail.com>
2025-10-02 16:50:37 +0100

    got a decent looking smooth graph, two lines, grid lines, and will work on more

--- src/script.js
+++ src/script.js
@@ -1,94 +1,19 @@
+let style = window.getComputedStyle(document.body);
+let mainColour = style.getPropertyValue('--main-colour');
+let subColour = style.getPropertyValue('--sub-colour');
+
 window.onload = function() {
     const fadeInElement = document.querySelectorAll('.fade-in');
     console.log(fadeInElement);
     for (const element of fadeInElement) {
         element.classList.add('show');
     }
-    const canvas = document.getElementById('graph');
-    const ctx = canvas.getContext('2d');
-    const width = canvas.width;
-    const height = canvas.height;
-    console.log(`Width: ${width}, height: ${height}`);
-    var style = window.getComputedStyle(document.body);
-    console.log(style.getPropertyValue('--main-colour'));
-
-    ctx.fillStyle = style.getPropertyValue('--main-colour');
-    const points = generateRandomPoints(8, width, height);
-    points.sort((a, b) => a[0] - b[0]);
-    console.log(points);
-    // We have sorted points along x
-    // Now we need to perform quadratic bezier curves on this
-    // so we grab tuples of 3 points, start, control, end
-    let finalPoints = [];
-    // for (let i = 0; i < points.length - 2; i++) {
-    //     const start = points[i];
-    //     const control = points[i + 1];
-    //     const end = points[i + 2];
-    //
-    //     for (let t = 0; t < 1.0; t += 0.1) {
-    //         const bezier = getPointOnQuadraticBezierCurveOptimised(start, control, end, t);
-    //         finalPoints.push(bezier);
-    //     }
-    // }
-    for (let i = 0; i < points.length - 1; i++) {
-        const p0 = points[Math.max(0, i - 1)];
-        const p1 = points[i];
-        const p2 = points[i + 1];
-        const p3 = points[Math.min(points.length - 1, i + 2)];
-
-        for (let t = 0; t < 1.0; t += 0.1) {
-            finalPoints.push(getCatmullRomPoint(p0, p1, p2, p3, t));
-        }
-    }
-    finalPoints.sort((a, b) => a[0] - b[0])
-    console.log("finalPoints",finalPoints);
-    // and we draw these points
-    ctx.lineWidth = 2;
-    ctx.lineJoin = "round";
-    ctx.lineCap = "round";
-    const mainColour = style.getPropertyValue('--main-colour');
-    const subColour = style.getPropertyValue('--sub-colour');
-    ctx.strokeStyle = mainColour;
-    ctx.beginPath();
-    ctx.moveTo(finalPoints[0][0], 200);
-    let maxY = 0;
-    for (const [x, y] of finalPoints) {
-        maxY = Math.max(maxY, y);
-        // ctx.strokeStyle=`oklch(${x} 0.5 120)`;
-        ctx.lineTo(x, y);
-    }
-    ctx.lineTo(finalPoints[finalPoints.length - 1][0], 200);
-    ctx.closePath();
-    ctx.fillStyle=`oklch(0.5117 0.2723 268.73 / 15%)`;
-    ctx.fill();
-
-    ctx.beginPath();
-    for (const [x, y] of finalPoints) {
-        ctx.lineTo(x,y);
-    }
-    ctx.stroke();
-    console.log(`Maxy: ${maxY}`);
-
-    const blockWidth = 8;
-    const blockHeight = 8;
-    // const { blocks, gradients } = generateAreaPoints(finalPoints, blockWidth, blockHeight);
-    // console.log("blocks", blocks);
-    // console.log("gradients", gradients);
-    // ctx.filter=`blur(${blockWidth > blockHeight ? blockWidth : blockHeight}px)`;
-    // ctx.filter=`blur(5px)`;
-    // for (let i = 0; i < blocks.length; i++) {
-    //     ctx.fillStyle=gradients[i];
-    //     ctx.fillRect(blocks[i][0], blocks[i][1], blockWidth, blockHeight);
-    // }
-
-    // ctx.fillRect(0, 0, 10, 10);
-
-    // ctx.fill
-    svgGraph();
+    svgGraph(subColour, "sub");
+    svgGraph(mainColour, "main");
 };
 
-function generateRandomPoints(amount, width, height) {
-    const array = Array.from({ length: amount }, (_, i) => [ (width / amount) * i, Number(((Math.random() * (height - 0)) + 0).toFixed(2))]);
+function generateRandomPoints(amount, width, height, minHeight) {
+    const array = Array.from({ length: amount }, (_, i) => [ (width / amount) * i, Number(((Math.random() * (height - minHeight)) + minHeight - (minHeight / 2)).toFixed(2))]);
     array.push([width, Number(((Math.random() * (height - 0)) + 0).toFixed(2))])
     return array;
 }
@@ -169,22 +94,23 @@ function getCatmullRomPoint(p0, p1, p2, p3, t) {
     ]
 }
 
-function svgGraph() {
+function svgGraph(colour, tag) {
     const svg = document.getElementById('svg-graph');
     const width = svg.clientWidth;
     const height = svg.clientHeight;
     console.log(`WIDTH: ${width}, height: ${height}`);
 
-    const existingCircles = svg.querySelectorAll('circle');
+
+    const existingCircles = svg.querySelectorAll(`.${tag}-svg-circle`);
+    console.log("existingCircles",existingCircles);
     existingCircles.forEach(circle => circle.remove());
 
-    var style = window.getComputedStyle(document.body);
-    var mainColour = style.getPropertyValue('--main-colour');
-    var subColour = style.getPropertyValue('--sub-colour');
+    const existingLine = svg.querySelectorAll(`.${tag}-line`);
+    existingLine.forEach((line) => line.remove());
 
-    const svgArea = document.getElementById('svg-area');
-    const svgPath = document.getElementById('svg-path');
-    const points = generateRandomPoints(8, width, height);
+    // const svgArea = document.getElementById('svg-area');
+    // const svgPath = document.getElementById('svg-path');
+    const points = generateRandomPoints(8, width, height, 50);
     console.log("SVG POINTS", points);
     points.sort((a, b) => a[0] - b[0]);
     let finalPoints = [];
@@ -200,15 +126,17 @@ function svgGraph() {
     }
     finalPoints.sort((a, b) => a[0] - b[0])
     console.log("SVG FINALPOINTS", finalPoints);
-    let path = `M 0 ${height}`;
-    for (const [x, y] of finalPoints) {
-        path += `L ${x} ${y} `;
-    }
 
-    path += `L ${width} ${height} Z`;
-
-    svgArea.setAttribute("fill", `oklch(0.5117 0.2723 268.73 / 15%)`);
-    svgArea.setAttribute("d", path);
+    // Fill
+    // let path = `M 0 ${height}`;
+    // for (const [x, y] of finalPoints) {
+    //     path += `L ${x} ${y} `;
+    // }
+    //
+    // path += `L ${width} ${height} Z`;
+    //
+    // svgArea.setAttribute("fill", `oklch(0.5117 0.2723 268.73 / 15%)`);
+    // svgArea.setAttribute("d", path);
 
     let pathLine = `M ${finalPoints[0][0]} ${finalPoints[0][1]}`;
     for (let i = 0; i < finalPoints.length; i++) {
@@ -216,23 +144,75 @@ function svgGraph() {
         pathLine += ` L ${x} ${y}`;
     }
 
-    svgPath.setAttribute("stroke", mainColour);
+    const svgPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+
+    svgPath.setAttribute("stroke", colour);
     svgPath.setAttribute("stroke-width", 2);
     svgPath.setAttribute("fill", "none");
+    svgPath.setAttribute("class", `${tag}-line`);
     svgPath.setAttribute("d", pathLine);
 
+    svg.appendChild(svgPath);
+
     points.forEach(([x, y], i) => {
-        if (i !== 0 && i !== points.length - 1) {
+        if (true) {
             const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
             circle.setAttribute("cx", x);
             circle.setAttribute("cy", y);
             circle.setAttribute("r", 5);
-            circle.setAttribute("fill", mainColour);
-            circle.setAttribute("class", "svg-circle");
+            circle.setAttribute("fill", colour);
+            circle.setAttribute("class", `${tag}-svg-circle`);
             svg.appendChild(circle);
+            const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+            if (i % 3 === 0 && tag === "main") {
+            text.innerHTML = `x: ${x}, y: ${y}`;
+            text.setAttribute("x", x - 20);
+            text.setAttribute("y", y - 20);
+            text.setAttribute("style", "font: italic 32px sans-serif;");
+            // svg.appendChild(text);
+            }
         }
-    })
+    });
+    const padding = 10;
+    const yPadding = 5;
+    const minX = Math.min(...finalPoints.map(p => p[0]));
+    const maxX = Math.max(...finalPoints.map(p => p[0]));
+    const minY = Math.min(...finalPoints.map(p => p[0]));
+    const maxY = Math.max(...finalPoints.map(p => p[0]));
+
+    const contentWidth = maxX - minX;
+    const contentHeight = maxY - minY;
+
+    svg.setAttribute('viewBox', 
+        `${-padding} -20 ${width + padding * 2} ${height + padding * 2}`
+    );
+    // svg.setAttribute('viewBox', 
+    //     `${minX - padding} ${minY - yPadding} ${contentWidth + padding * 2} ${contentHeight + yPadding * 2}`
+    // );
+
+    drawGridLines();
 
 }
 
-addEventListener("resize", svgGraph);
+function drawGridLines() {
+    const svg = document.getElementById('svg-graph');
+    const width = svg.clientWidth;
+    const height = svg.clientHeight;
+    const existingLines = svg.querySelectorAll('.svg-grid-line');
+    console.log("existsingLines", existingLines);
+    existingLines.forEach(line => line.remove());
+    const noLines = height / 4;
+    for (let i = 0; i < noLines; i++) {
+        const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        line.setAttribute("x1", 0);
+        line.setAttribute("x2", width);
+        line.setAttribute("y1", (height / 4) * i);
+        line.setAttribute("y2", (height / 4) * i);
+        line.setAttribute("stroke-width", 1);
+        line.setAttribute("stroke", mainColour);
+        line.setAttribute("class", "svg-grid-line");
+        svg.appendChild(line);
+    }
+}
+
+addEventListener("resize", () => {svgGraph(subColour, "sub"); svgGraph(mainColour, "main") });
